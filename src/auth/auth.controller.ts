814b8e43fe5055c9137a3e9f1be4511b9f9b1076
Carlos Gimenez <krlito.gb@gmail.com>
2025-02-17 15:38:21 -0300

    Autentication - JWT - Autorization part 1

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,27 @@
+import { Body, Controller, Get, HttpCode, HttpStatus, Post, Request, UseGuards } from '@nestjs/common';
+import { LoginDto } from './dto/login.dto';
+import { RegisterDto } from './dto/register.dto';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './guard/auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  register(@Body() registerDto: RegisterDto) {
+    return this.authService.register(registerDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Get('Profile')
+  @UseGuards(AuthGuard)
+  profile(@Request() req){
+   return req.user;
+  }
+}
